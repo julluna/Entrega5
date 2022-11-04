3d9afeb8e446a063cdd9cba11316e8847b5d9eda
user <user@MacBook-Pro-de-user-2.local>
2022-11-03 23:02:02 -0300

    terminado

---
+++ ejs/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = require('./routes/products');
+const PORT = 8081;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/', router);
+
+const server = app.listen(PORT, () =>
+	console.log(`Server running on port ${PORT}`)
+);
+
+server.on('error', () => console.log("Error"));
